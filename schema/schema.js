b9e2064c811d551cc1eda9d103a9dd9f2f5af9ea
Lindsay Xue <lingzixue113@gmail.com>
2021-08-01 16:42:25 +1200

    feat: first schema

---
+++ schema/schema.js
@@ -0,0 +1,58 @@
+const _ = require('lodash');
+const graphql = require('graphql');
+
+const users = [
+    {
+        id: '23',
+        firstName: 'Bill',
+        age: '20'
+    },
+    {
+        id: '47',
+        firstName: 'Samantha',
+        age: '40'
+    }
+]
+
+const {
+    GraphQLObjectType,
+    GraphQLString,
+    GraphQLInt,
+    GraphQLSchema
+} = graphql;
+
+const UserType = new GraphQLObjectType({
+    name: 'User',
+    fields: {
+        id: {
+            type: GraphQLString
+        },
+        firstName: {
+            type: GraphQLString
+        },
+        age: {
+            type: GraphQLInt
+        }
+    }
+});
+
+const RootQuery = new GraphQLObjectType({
+    name: 'RootQueryType',
+    fields: {
+        user: {
+            type: UserType,
+            args: {
+                id: {
+                    type: GraphQLString
+                }
+            },
+            resolve(parentValue, args) {
+                return _.find(users, { id: args.id })
+            }
+        }
+    }
+});
+
+module.exports = new GraphQLSchema({
+    query: RootQuery
+});
